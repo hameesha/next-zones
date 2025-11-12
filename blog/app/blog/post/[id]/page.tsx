06ab1e823a5f686513e9817637bc04bdfb7f4a36
Hameesha Rantharu <45784711+hameesha@users.noreply.github.com>
2025-11-12 13:29:35 +0530

    Update page.tsx

--- blog/app/blog/post/[id]/page.tsx
+++ blog/app/blog/post/[id]/page.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 
-export default function Post({ params: { id } }) {
+export default function Post({ params: { id } }:{
+  params: {
+    id: string;
+  };
+}) {
   return (
     <div>
       <h3>Post #{id}</h3>
